57faf09e4faab1e30fb5284da8c0ba59873144cc
robot171 <christopher.paul001@gmail.com>
2017-07-25 10:16:42 +0200

    working basic header & page before adding blog list

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,11 @@ from django.views.generic import ListView, DetailView
 from blog.models import PropagandistSlogans
 
 urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
+
+"""
+urlpatterns = [
     #url(r'^$', views.index, name='index'),
     url(r'^$', ListView.as_view(
         queryset=PropagandistSlogans.objects.all()[:2],
@@ -14,23 +19,4 @@ urlpatterns = [
         template_name="blog/home.html")),
 ]
 
-
-
-
-
-
-"""
-from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
-from blog.models import Post
-
-urlpatterns = [
-    url(r'^$', ListView.as_view(
-        queryset=Post.objects.all().order_by("-date")[:25],
-        template_name="blog/blog.html")),
-
-    url(r'^(?P<pk>\d+)$', DetailView.as_view(
-        model=Post,
-        template_name="blog/post.html")),
-]
 """
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'blog/home.html')
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 
